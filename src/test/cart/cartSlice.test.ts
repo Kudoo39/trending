6187d6aab2a6b32297d9c84176ad0157d538ae28
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2024-04-28 15:31:55 +0300

    rewriting all test cases for cartSlice, categorySlice, productSlice & userSlice

--- src/test/cart/cartSlice.test.ts
+++ src/test/cart/cartSlice.test.ts
@@ -1,81 +1,171 @@
-// /* eslint-disable no-undef */
-// import cartReducer, { addToCart, removeFromCart, updateQuantity, clearCart } from '../../redux/slices/cartSlice'
+/* eslint-disable no-undef */
+import cartReducer, { addToCart, removeFromCart, updateQuantity, clearCart, addOrderByUserId, getOrderByUserId } from '../../redux/slices/cartSlice'
 
-// const initialState = {
-//   cart: [
-//     {
-//       id: 1,
-//       title: 'Product 1',
-//       price: 10,
-//       description: 'Description 1',
-//       category: { id: 1, name: 'Category 1', image: 'Image 1' },
-//       images: ['Image 1'],
-//       quantity: 2
-//     },
-//     {
-//       id: 2,
-//       title: 'Product 2',
-//       price: 20,
-//       description: 'Description 2',
-//       category: { id: 2, name: 'Category 2', image: 'Image 2' },
-//       images: ['Image 2'],
-//       quantity: 4
-//     }
-//   ]
-// }
+const initialState = {
+  cart: [
+    {
+      _id: '1',
+      title: 'Product 1',
+      price: 10,
+      description: 'Description 1',
+      categoryId: { _id: '1', name: 'Category 1', image: 'Image 1' },
+      image: 'Image 1',
+      quantity: 2
+    },
+    {
+      _id: '2',
+      title: 'Product 2',
+      price: 20,
+      description: 'Description 2',
+      categoryId: { _id: '2', name: 'Category 2', image: 'Image 2' },
+      image: 'Image 2',
+      quantity: 4
+    }
+  ],
+  orders: [],
+  loading: false,
+  error: null
+}
 
-// const mockItem = {
-//   id: 3,
-//   title: 'Product 3',
-//   price: 30,
-//   description: 'Description 3',
-//   category: { id: 3, name: 'Category 3', image: 'Image 3' },
-//   images: ['Image 3'],
-//   quantity: 3
-// }
+const mockItem = {
+  _id: '3',
+  title: 'Product 3',
+  price: 30,
+  description: 'Description 3',
+  categoryId: { _id: '3', name: 'Category 3', image: 'Image 3' },
+  image: 'Image 3',
+  quantity: 3
+}
 
-// describe('cartSlice reducers', () => {
-//   test('should add items to the cart', () => {
-//     const action = addToCart(mockItem)
-//     const nextState = cartReducer(initialState, action)
+const mockOrder = {
+  products: [
+    { productId: 'product1', quantity: 2 },
+    { productId: 'product2', quantity: 1 }
+  ]
+}
 
-//     expect(nextState.cart.length).toBe(3)
-//   })
+describe('cartSlice reducers', () => {
+  test('should add items to the cart', () => {
+    const action = addToCart(mockItem)
+    const nextState = cartReducer(initialState, action)
 
-//   test('should remove items from the cart', () => {
-//     const action = removeFromCart(1)
-//     const nextState = cartReducer(initialState, action)
+    expect(nextState.cart.length).toBe(3)
+  })
 
-//     expect(nextState.cart.length).toBe(1)
-//     expect(nextState.cart.find(item => item.id === 1)).toBeUndefined()
-//   })
+  test('should remove items from the cart', () => {
+    const action = removeFromCart('1')
+    const nextState = cartReducer(initialState, action)
 
-//   test('should not remove any items if the item is not in the cart', () => {
-//     const action = removeFromCart(100)
-//     const nextState = cartReducer(initialState, action)
+    expect(nextState.cart.length).toBe(1)
+    expect(nextState.cart.find(item => item._id === '1')).toBeUndefined()
+  })
 
-//     expect(nextState.cart.length).toBe(2)
-//   })
+  test('should not remove any items if the item is not in the cart', () => {
+    const action = removeFromCart('100')
+    const nextState = cartReducer(initialState, action)
 
-//   test('should update quantity of an item in the cart', () => {
-//     const action = updateQuantity({ id: 1, quantity: 2 })
-//     const nextState = cartReducer(initialState, action)
+    expect(nextState.cart.length).toBe(2)
+  })
 
-//     expect(nextState.cart[0].quantity).toBe(4)
-//   })
+  test('should update quantity of an item in the cart', () => {
+    const action = updateQuantity({ _id: '1', quantity: 2 })
+    const nextState = cartReducer(initialState, action)
 
-//   test('should not update quantity if item does not exist in the cart', () => {
-//     const updateAction = updateQuantity({ id: 3, quantity: 3 })
-//     const nextState = cartReducer(initialState, updateAction)
+    expect(nextState.cart[0].quantity).toBe(4)
+  })
 
-//     expect(nextState).toEqual(initialState)
-//   })
+  test('should not update quantity if item does not exist in the cart', () => {
+    const updateAction = updateQuantity({ _id: '3', quantity: 3 })
+    const nextState = cartReducer(initialState, updateAction)
 
-//   test('should clear the cart', () => {
-//     const action = clearCart()
-//     const nextState = cartReducer(initialState, action)
+    expect(nextState).toEqual(initialState)
+  })
 
-//     expect(nextState.cart).toEqual([])
-//     expect(nextState.cart.length).toBe(0)
-//   })
-// })
+  test('should clear the cart', () => {
+    const action = clearCart()
+    const nextState = cartReducer(initialState, action)
+
+    expect(nextState.cart).toEqual([])
+    expect(nextState.cart.length).toBe(0)
+  })
+
+  // test 1: addOrderByUserId fulfill
+  test('should add order by user id', () => {
+    const action = addOrderByUserId.fulfilled(mockItem, 'fulfilled', { userId: '1', orders: mockOrder })
+    const nextState = cartReducer(initialState, action)
+
+    expect(nextState).toEqual({
+      cart: initialState.cart,
+      orders: [],
+      loading: false,
+      error: null
+    })
+  })
+
+  // test 2: addOrderByUserId pending
+  test('should have loading truthy when order is adding', () => {
+    const action = addOrderByUserId.pending('pending', { userId: '1', orders: mockOrder })
+    const nextState = cartReducer(initialState, action)
+
+    expect(nextState).toEqual({
+      cart: initialState.cart,
+      orders: [],
+      loading: true,
+      error: null
+    })
+  })
+
+  // test 3: addOrderByUserId error
+  test('should show error when adding order failed', () => {
+    const error = new Error('error')
+    const action = addOrderByUserId.rejected(error, 'error', { userId: '1', orders: mockOrder })
+    const nextState = cartReducer(initialState, action)
+
+    expect(nextState).toEqual({
+      cart: initialState.cart,
+      orders: [],
+      loading: false,
+      error: error.message
+    })
+  })
+
+  // test 4: getOrderByUserId fulfill
+  test('should get order by user id', () => {
+    const action = getOrderByUserId.fulfilled(mockItem, 'fulfilled', '1')
+    const nextState = cartReducer(initialState, action)
+
+    expect(nextState).toEqual({
+      cart: initialState.cart,
+      orders: mockItem,
+      loading: false,
+      error: null
+    })
+  })
+
+  // test 5: getOrderByUserId pending
+  test('should have loading truthy when order is fetching', () => {
+    const action = getOrderByUserId.pending('fulfilled', '1')
+    const nextState = cartReducer(initialState, action)
+
+    expect(nextState).toEqual({
+      cart: initialState.cart,
+      orders: [],
+      loading: true,
+      error: null
+    })
+  })
+
+  // test 6: getOrderByUserId error
+  test('should show error when get orders failed', () => {
+    const error = new Error('error')
+    const action = getOrderByUserId.rejected(error, 'error', '1')
+    const nextState = cartReducer(initialState, action)
+
+    expect(nextState).toEqual({
+      cart: initialState.cart,
+      orders: [],
+      loading: false,
+      error: error.message
+    })
+  })
+})
--- src/test/user/userSlice.test.ts
+++ src/test/user/userSlice.test.ts
@@ -1,194 +1,492 @@
-// /* eslint-disable no-undef */
-// import { User, UserCredential, UserRegister } from '../../misc/type'
-// import userReducer, {
-//   logout,
-//   loginUserAsync,
-//   registerUserAsync,
-//   authenticateUserAsync
-// } from '../../redux/slices/userSlice'
+/* eslint-disable no-undef */
+import { User, UserCredential, UserRegister } from '../../misc/type'
+import userReducer, {
+  logout,
+  loginUserAsync,
+  registerUserAsync,
+  authenticateUserAsync,
+  fetchUsersAsync,
+  updateUserProfileAsync,
+  updateUserPasswordAsync
+} from '../../redux/slices/userSlice'
 
-// type InitialState = {
-//   users: User[]
-//   user?: User | null
-//   loading: boolean
-//   error: string | null
-//   isAuthenticated: boolean
-// }
+type InitialState = {
+  users: User[]
+  user?: User | null
+  loading: boolean
+  error: string | null
+  isAuthenticated: boolean
+}
 
-// describe('userSlice reducers', () => {
-//   test('should log out from the account', () => {
-//     const initialState: InitialState = {
-//       users: [],
-//       user: {
-//         id: 1,
-//         email: 'user1@mail.com',
-//         name: 'User 1',
-//         role: 'admin',
-//         avatar: 'https://i.imgur.com/LDOO4Qs.jpg',
-//         password: 'password1'
-//       },
-//       loading: false,
-//       error: null,
-//       isAuthenticated: true
-//     }
+const initialState: InitialState = {
+  users: [],
+  user: null,
+  loading: false,
+  error: null,
+  isAuthenticated: false
+}
 
-//     const action = logout()
-//     const nextState = userReducer(initialState, action)
+const mockUsers: User[] = [
+  {
+    email: 'user1@example.com',
+    password: 'password1',
+    firstname: 'User',
+    lastname: '1',
+    avatar: 'image1',
+    role: 'customer',
+    _id: '1',
+    orders: [],
+    banStatus: false
+  },
+  {
+    email: 'user2@example.com',
+    password: 'password2',
+    firstname: 'User',
+    lastname: '2',
+    avatar: 'image2',
+    role: 'customer',
+    _id: '2',
+    orders: [],
+    banStatus: false
+  }
+]
 
-//     expect(nextState.user).toBeNull
-//     expect(nextState.isAuthenticated).toBeFalsy()
-//   })
+const mockSingleUser: User =
+  {
+    email: 'user3@example.com',
+    password: 'password3',
+    firstname: 'User',
+    lastname: '3',
+    avatar: 'image3',
+    role: 'customer',
+    _id: '3',
+    orders: [],
+    banStatus: false
+  }
 
-//   // test 0: initial state
-//   test('should return initial state', () => {
-//     const initialState: InitialState = {
-//       users: [],
-//       loading: false,
-//       error: null,
-//       isAuthenticated: false
-//     }
 
-//     const nextState = userReducer(undefined, { type: '' })
+describe('userSlice reducers', () => {
+  test('should log out from the account', () => {
+    const initialState: InitialState = {
+      users: [],
+      user: {
+        _id: '1',
+        email: 'user1@mail.com',
+        firstname: 'User',
+        lastname: '1',
+        role: 'admin',
+        avatar: 'https://i.imgur.com/LDOO4Qs.jpg',
+        password: 'password1',
+        banStatus: false,
+        orders: [
+          {
+            products: [
+              { productId: 'product1', quantity: 2 },
+              { productId: 'product2', quantity: 1 }
+            ]
+          }]
+      },
+      loading: false,
+      error: null,
+      isAuthenticated: true
+    }
 
-//     expect(nextState).toEqual(initialState)
-//   })
+    const action = logout()
+    const nextState = userReducer(initialState, action)
 
-//   // test 1: register fulfill
-//   test('should register an account', () => {
-//     const initialState: InitialState = {
-//       users: [],
-//       user: null,
-//       loading: false,
-//       error: null,
-//       isAuthenticated: false
-//     }
+    expect(nextState.user).toBeNull
+    expect(nextState.isAuthenticated).toBeFalsy()
+  })
 
-//     const registerUser: UserRegister = {
-//       email: 'user2@mail.com',
-//       name: 'User 2',
-//       password: 'password2',
-//       avatar: 'https://i.imgur.com/LDOO4Qs.jpg'
-//     }
+  // test 0: initial state
+  test('should return initial state', () => {
+    const initialState: InitialState = {
+      users: [],
+      loading: false,
+      error: null,
+      isAuthenticated: false
+    }
 
-//     const action = registerUserAsync.fulfilled(registerUser, 'fulfilled', registerUser)
-//     const nextState = userReducer(initialState, action)
+    const nextState = userReducer(undefined, { type: '' })
 
-//     expect(nextState.user).toEqual(registerUser)
-//     expect(nextState.loading).toBe(false)
-//     expect(nextState.error).toBeNull()
-//     expect(nextState.isAuthenticated).toBeFalsy()
-//   })
+    expect(nextState).toEqual(initialState)
+  })
 
-//   // test 2: register pending
-//   test('should have loading truthy when fetch is pending', () => {
-//     const initialState: InitialState = {
-//       users: [],
-//       user: null,
-//       loading: false,
-//       error: null,
-//       isAuthenticated: false
-//     }
+  // test 1: register fulfill
+  test('should register an account', () => {
+    const registerUser: UserRegister = {
+      email: 'user2@mail.com',
+      firstname: 'User',
+      lastname: '2',
+      password: 'password2',
+      avatar: 'https://i.imgur.com/LDOO4Qs.jpg'
+    }
 
-//     const registerUser: UserRegister = {
-//       email: 'user3@mail.com',
-//       name: 'User 3',
-//       password: 'password3',
-//       avatar: 'https://i.imgur.com/LDOO4Qs.jpg'
-//     }
+    const action = registerUserAsync.fulfilled(registerUser, 'fulfilled', registerUser)
+    const nextState = userReducer(initialState, action)
 
-//     const action = registerUserAsync.pending('pending', registerUser)
-//     const nextState = userReducer(initialState, action)
+    expect(nextState.user).toEqual(registerUser)
+    expect(nextState.loading).toBe(false)
+    expect(nextState.error).toBeNull()
+    expect(nextState.isAuthenticated).toBeFalsy()
+  })
 
-//     expect(nextState.user).toBeNull
-//     expect(nextState.loading).toBe(true)
-//     expect(nextState.error).toBeNull()
-//     expect(nextState.isAuthenticated).toBeFalsy()
-//   })
+  // test 2: register pending
+  test('should have loading truthy when fetch is pending', () => {
+    const registerUser: UserRegister = {
+      email: 'user3@mail.com',
+      firstname: 'User',
+      lastname: '3',
+      password: 'password3',
+      avatar: 'https://i.imgur.com/LDOO4Qs.jpg'
+    }
 
-//   // test 3: register error
-//   test('should have error', () => {
-//     const initialState: InitialState = {
-//       users: [],
-//       user: null,
-//       loading: false,
-//       error: null,
-//       isAuthenticated: false
-//     }
+    const action = registerUserAsync.pending('pending', registerUser)
+    const nextState = userReducer(initialState, action)
 
-//     const registerUser: UserRegister = {
-//       email: 'user3@mail.com',
-//       name: 'User 3',
-//       password: 'password3',
-//       avatar: 'https://i.imgur.com/LDOO4Qs.jpg'
-//     }
+    expect(nextState.user).toBeNull
+    expect(nextState.loading).toBe(true)
+    expect(nextState.error).toBeNull()
+    expect(nextState.isAuthenticated).toBeFalsy()
+  })
 
-//     const error = new Error('error')
-//     const action = registerUserAsync.rejected(error, 'error', registerUser)
-//     const nextState = userReducer(initialState, action)
+  // test 3: register error
+  test('should have error', () => {
+    const registerUser: UserRegister = {
+      email: 'user4@mail.com',
+      firstname: 'User',
+      lastname: '4',
+      password: 'password4',
+      avatar: 'https://i.imgur.com/LDOO4Qs.jpg'
+    }
 
-//     expect(nextState.user).toBeNull
-//     expect(nextState.loading).toBe(false)
-//     expect(nextState.error).toEqual(error.message)
-//     expect(nextState.isAuthenticated).toBeFalsy()
-//   })
+    const error = new Error('error')
+    const action = registerUserAsync.rejected(error, 'error', registerUser)
+    const nextState = userReducer(initialState, action)
 
-//   // test 4: login fulfill
-//   test('should login an account', () => {
-//     const initialState: InitialState = {
-//       users: [],
-//       user: null,
-//       loading: false,
-//       error: null,
-//       isAuthenticated: false
-//     }
+    expect(nextState.user).toBeNull
+    expect(nextState.loading).toBe(false)
+    expect(nextState.error).toEqual(error.message)
+    expect(nextState.isAuthenticated).toBeFalsy()
+  })
 
-//     const loginUser: UserCredential = {
-//       email: 'user4@mail.com',
-//       password: 'password4'
-//     }
+  // test 4: login fulfill
+  test('should login an account', () => {
+    const loginUser: UserCredential = {
+      email: 'user5@mail.com',
+      password: 'password5'
+    }
 
-//     const successfulLoginUser: User = {
-//       ...loginUser,
-//       id: 1,
-//       name: 'User 4',
-//       role: 'customer',
-//       avatar: 'https://i.imgur.com/LDOO4Qs.jpg'
-//     }
+    const successfulLoginUser: User = {
+      ...loginUser,
+      _id: '5',
+      firstname: 'User',
+      lastname: '5',
+      role: 'customer',
+      avatar: 'https://i.imgur.com/LDOO4Qs.jpg',
+      banStatus: false,
+      orders: [
+        {
+          products: [
+            { productId: 'product1', quantity: 2 },
+            { productId: 'product2', quantity: 1 }
+          ]
+        }]
+    }
 
-//     const action = loginUserAsync.fulfilled(successfulLoginUser, 'fulfilled', loginUser)
-//     const nextState = userReducer(initialState, action)
+    const action = loginUserAsync.fulfilled(successfulLoginUser, 'fulfilled', loginUser)
+    const nextState = userReducer(initialState, action)
 
-//     expect(nextState.user).toEqual(successfulLoginUser)
-//     expect(nextState.loading).toBe(false)
-//     expect(nextState.error).toBeNull()
-//     expect(nextState.isAuthenticated).toBeFalsy()
-//   })
+    expect(nextState.user).toEqual(successfulLoginUser)
+    expect(nextState.loading).toBe(false)
+    expect(nextState.error).toBeNull()
+    expect(nextState.isAuthenticated).toBeFalsy()
+    expect(nextState).toEqual({
+      users: [],
+      user: successfulLoginUser,
+      loading: false,
+      error: null,
+      isAuthenticated: false
+    })
+  })
 
-//   // test 5: authenticate fulfill
-//   test('should authenticate an account', () => {
-//     const initialState: InitialState = {
-//       users: [],
-//       user: null,
-//       loading: false,
-//       error: null,
-//       isAuthenticated: false
-//     }
+  // test 4.1: login pending
+  test('should handle loading when login an account', () => {
+    const loginUser: UserCredential = {
+      email: 'user5@mail.com',
+      password: 'password5'
+    }
 
-//     const authenticationData: User = {
-//       id: 1,
-//       email: 'user5@mail.com',
-//       name: 'User 5',
-//       role: 'customer',
-//       avatar: 'https://i.imgur.com/LDOO4Qs.jpg',
-//       password: 'password5'
-//     }
+    const action = loginUserAsync.pending('pending', loginUser)
+    const nextState = userReducer(initialState, action)
 
-//     const action = authenticateUserAsync.fulfilled(authenticationData, 'fulfilled', 'fulfilled')
-//     const nextState = userReducer(initialState, action)
+    expect(nextState.user).toBeNull()
+    expect(nextState.loading).toBe(true)
+    expect(nextState.error).toBeNull()
+    expect(nextState.isAuthenticated).toBeFalsy()
+    expect(nextState).toEqual({
+      users: [],
+      user: null,
+      loading: true,
+      error: null,
+      isAuthenticated: false
+    })
+  })
 
-//     expect(nextState.user).toEqual(authenticationData)
-//     expect(nextState.loading).toBe(false)
-//     expect(nextState.error).toBeNull()
-//     expect(nextState.isAuthenticated).toBeTruthy()
-//   })
-// })
+  // test 4.2: login error
+  test('should handle loading when login an account', () => {
+    const loginUser: UserCredential = {
+      email: 'user5@mail.com',
+      password: 'password5'
+    }
+
+    const error = new Error('error')
+    const action = loginUserAsync.rejected(error, 'error', loginUser)
+    const nextState = userReducer(initialState, action)
+
+    expect(nextState).toEqual({
+      users: [],
+      user: null,
+      loading: false,
+      error: error.message,
+      isAuthenticated: false
+    })
+  })
+
+  // test 5: authenticate fulfill
+  test('should authenticate an account', () => {
+    const authenticationData: User = {
+      _id: '6',
+      email: 'user6@mail.com',
+      firstname: 'User',
+      lastname: '6',
+      role: 'customer',
+      avatar: 'https://i.imgur.com/LDOO4Qs.jpg',
+      password: 'password6',
+      banStatus: false,
+      orders: [
+        {
+          products: [
+            { productId: 'product1', quantity: 2 },
+            { productId: 'product2', quantity: 1 }
+          ]
+        }]
+    }
+
+    const action = authenticateUserAsync.fulfilled(authenticationData, 'fulfilled', 'fulfilled')
+    const nextState = userReducer(initialState, action)
+
+    expect(nextState.user).toEqual(authenticationData)
+    expect(nextState.loading).toBe(false)
+    expect(nextState.error).toBeNull()
+    expect(nextState.isAuthenticated).toBeTruthy()
+  })
+})
+
+// test 6: fetchUsersAsync fulfill
+test('should return a list of users', () => {
+  const action = fetchUsersAsync.fulfilled(mockUsers, 'fulfilled')
+  const nextState = userReducer(initialState, action)
+
+  expect(nextState).toEqual({
+    users: [...mockUsers],
+    user: null,
+    loading: false,
+    error: null,
+    isAuthenticated: false
+  })
+})
+
+// test 7: fetchUsersAsync pending
+test('should have loading truthy when fetch is pending', () => {
+  const action = fetchUsersAsync.pending('pending')
+  const nextState = userReducer(initialState, action)
+
+  expect(nextState).toEqual({
+    users: [],
+    user: null,
+    loading: true,
+    error: null,
+    isAuthenticated: false
+  })
+})
+
+// test 8: fetchUsersAsync error
+test('should have error when fetch users', () => {
+  const error = new Error('error')
+  const action = fetchUsersAsync.rejected(error, 'error')
+  const nextState = userReducer(initialState, action)
+
+  expect(nextState).toEqual({
+    users: [],
+    user: null,
+    loading: false,
+    error: error.message,
+    isAuthenticated: false
+  })
+})
+
+// test 9: updateUserProfileAsync fulfill
+test('should handle update user profile', () => {
+  const initialStateWithUser = {
+    ...initialState,
+    users: [mockSingleUser]
+  }
+
+  const updatedUser = {
+    ...mockSingleUser,
+    firstname: 'User update',
+    lastname: '3'
+  }
+  const action = updateUserProfileAsync.fulfilled(updatedUser, 'fulfilled', {
+    updateUser: updatedUser,
+    userId: updatedUser._id
+  })
+  const nextState = userReducer(initialStateWithUser, action)
+
+  expect(nextState).toEqual({
+    users: [updatedUser],
+    user: null,
+    loading: false,
+    error: null,
+    isAuthenticated: false
+  })
+})
+
+// test 10: updateUserProfileAsync pending
+test('should handle loading when update user profile', () => {
+  const initialStateWithUser = {
+    ...initialState,
+    users: [mockSingleUser]
+  }
+
+  const updatedUser = {
+    ...mockSingleUser,
+    firstname: 'User update',
+    lastname: '3'
+  }
+  const action = updateUserProfileAsync.pending('pending', {
+    updateUser: updatedUser,
+    userId: updatedUser._id
+  })
+  const nextState = userReducer(initialStateWithUser, action)
+
+  expect(nextState).toEqual({
+    users: [mockSingleUser],
+    user: null,
+    loading: true,
+    error: null,
+    isAuthenticated: false
+  })
+})
+
+// test 11: updateUserProfileAsync error
+test('should handle errors when update user profile fail', () => {
+  const initialStateWithUser = {
+    ...initialState,
+    users: [mockSingleUser]
+  }
+
+  const updatedUser = {
+    ...mockSingleUser,
+    firstname: 'User update',
+    lastname: '3'
+  }
+
+  const error = new Error('error')
+  const action = updateUserProfileAsync.rejected(error, 'error', {
+    updateUser: updatedUser,
+    userId: updatedUser._id
+  })
+  const nextState = userReducer(initialStateWithUser, action)
+
+  expect(nextState).toEqual({
+    users: [mockSingleUser],
+    user: null,
+    loading: false,
+    error: error.message,
+    isAuthenticated: false
+  })
+})
+
+// test 12: updateUserPasswordAsync fulfill
+test('should handle update user profile', () => {
+  const initialStateWithUser = {
+    ...initialState,
+    users: [mockSingleUser]
+  }
+
+  const updatedUser = {
+    ...mockSingleUser,
+    email: 'user3@example.com',
+    password: 'password3update',
+    newPassword: 'password3update'
+  }
+  const action = updateUserPasswordAsync.fulfilled(updatedUser, 'fulfilled', {
+    updatePassword: updatedUser
+  })
+  const nextState = userReducer(initialStateWithUser, action)
+  updatedUser.password = 'password3update'
+
+  expect(nextState).toEqual({
+    users: [mockSingleUser],
+    user: null,
+    loading: false,
+    error: null,
+    isAuthenticated: false
+  })
+})
+
+// test 13: updateUserPasswordAsync pending
+test('should handle loading when update user profile', () => {
+  const initialStateWithUser = {
+    ...initialState,
+    users: [mockSingleUser]
+  }
+
+  const updatedUser = {
+    ...mockSingleUser,
+    password: 'password3',
+    newPassword: 'passwordupdate'
+  }
+  const action = updateUserPasswordAsync.pending('pending', {
+    updatePassword: updatedUser
+  })
+  const nextState = userReducer(initialStateWithUser, action)
+
+  expect(nextState).toEqual({
+    users: [mockSingleUser],
+    user: null,
+    loading: true,
+    error: null,
+    isAuthenticated: false
+  })
+})
+
+// test 14: updateUserPasswordAsync error
+test('should handle errors when update user profile fail', () => {
+  const initialStateWithUser = {
+    ...initialState,
+    users: [mockSingleUser]
+  }
+
+  const updatedUser = {
+    ...mockSingleUser,
+    password: 'password3',
+    newPassword: 'passwordupdate'
+  }
+
+  const error = new Error('error')
+  const action = updateUserPasswordAsync.rejected(error, 'error', {
+    updatePassword: updatedUser
+  })
+  const nextState = userReducer(initialStateWithUser, action)
+
+  expect(nextState).toEqual({
+    users: [mockSingleUser],
+    user: null,
+    loading: false,
+    error: error.message,
+    isAuthenticated: false
+  })
+})
